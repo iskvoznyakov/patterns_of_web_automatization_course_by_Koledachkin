c5b1ce2086b9acd16fc96f378ab93fed31e1dece
Ilya Skvoznyakov <104623164+iskvoznyakov@users.noreply.github.com>
2025-04-24 20:13:37 +0400

    added two login tests in imperative and declarative ways; and a method in  login_page.py (#6)

--- tests/test_login_and_post.py
+++ tests/test_login_and_post.py
@@ -26,3 +26,17 @@ class TestLoginAndHomePages(BaseTest):
         self.login_page.enter_password("invalid_password")
         self.login_page.click_login_button()
         assert self.login_page.is_error_message_visible()
+
+    @allure.title("Valid login in imperative way")
+    def test_successful_login_imperative(self):
+        self.login_page.open()
+        self.login_page.enter_login("administrator")
+        self.login_page.enter_password("administrator")
+        self.login_page.click_login_button()
+        assert self.login_page.driver.current_url == "https://demo.opensource-socialnetwork.org/home"
+
+    @allure.title("Valid login in declarative way")
+    def test_successful_login_declarative(self):
+        self.login_page.open()
+        self.login_page.login_as("administrator", "administrator")
+        assert self.login_page.driver.current_url == "https://demo.opensource-socialnetwork.org/home"
